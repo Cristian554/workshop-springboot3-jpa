281605d448333c579ea382c55339c2dd3e5d2136
Cristian554 <cristianpinto256@gmail.com>
2024-09-04 08:39:08 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/educandoweb/course/resources/exceptions/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -22,4 +23,11 @@ public class ResourceExceptionHandler {
 		return ResponseEntity.status(status).body(err);// Status que passa a mensagem ppersonalizada da excessão
 	}
 	
+	@ExceptionHandler(DataBaseException.class) // Anotação para interceptar excessão
+	 public ResponseEntity<StandardError> dataBase(DataBaseException e, HttpServletRequest request){
+		String error = "Data Base Error"; 
+		HttpStatus status = HttpStatus.BAD_REQUEST; 
+		StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI()); // Instanciando o StandarError
+		return ResponseEntity.status(status).body(err);// Status que passa a mensagem ppersonalizada da excessão
+	}
 }
--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.educandoweb.course.enitties.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DataBaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service // Anotation utilizado para registrar serviços
@@ -30,8 +32,15 @@ public class UserService {
 		return repository.save(obj);
 	}
 
-	public void delete(Long id) {
-		repository.deleteById(id);
+	public void delete(Long id){
+	    try {
+	        if(!repository.existsById(id)) throw new ResourceNotFoundException(id);
+	        repository.deleteById(id);
+	    } catch (ResourceNotFoundException e){
+	        throw new ResourceNotFoundException(id);
+	    } catch(DataIntegrityViolationException e) {
+	    	throw new DataBaseException(e.getMessage());
+	    }
 	}
 
 	public User update(Long id, User obj) {
